9544e8f13adb283f85e897b27694fa3a15eeb7d3
oyascode <johnmikeloy@gmail.com>
2021-03-12 05:37:34 +0100

    statement and expression readme

---
+++ README.md
@@ -0,0 +1,55 @@
+# JavaScript Statements  
+JavaScript statements are simply a unit of instructions that point out strictly an action that a computer should carry out. They, unlike properties, methods and events are independent of any JavaScript object. This makes them usable in windows object, document object and any other form of object.
+
+## Examples of JavaScript Statements
+Some examples of JavaScrfipt statements include:  
+### Conditional Statement
+  if (epression)  
+      Do something  
+  else  
+      Do another thing  
+### Declaration Statement
+const jump;  
+function jumpTheGun() {  
+  Do something  
+}  
+### Loop
+Do  
+break dance  
+while(expression)
+
+# JavaScript Expressions
+An expression in JavaScript is a unit of code that can be evaluated or resolved to a Value.
+
+## Examples of JavaScript Expressions
+Below are a few examples:  
+### Primary expression
+e.g True  
+    "Hello World"
+### Arithmetic Expression
+e.g 15 + 3
+### String Expression
+e.g "Beautiful " + "Music"
+### Logical Expression
+e.g animal === "dog"
+
+# Difference Between Statement and Expression
+
+Programs are practically sequence of statements. Therefore, a statment can be written where an expression is expected-in this case, it will be an expression statement.
+
+Whereas Expressions generally produce a value, they are therefore used when we expect to get a value.  As a result, an expression cannot be written when a statement is expected.
+
+
+# What are JavaScript functions
+
+Functions are a set of statement that carry out a specific task. They help us to avoid unnecessary repetition of codes as they can be used over and over again.  
+
+For example, to "eat out" is popularly used to mean:  
+1. Go to a restaurant
+2. buy a meal of interest  
+
+so we do not have to constantly say I want to go to a restaurant to eat a particular meal but can simply say I want to "eat out"
+
+
+
+
--- script.js
+++ script.js
@@ -14,85 +14,85 @@ const computeSimpleMath = (firstNum, secondNum, myString) => {
   }
 }
 //Invoke function with arguments and log to the console
-console.log(computeSimpleMath(20, 5, "sum"))
+console.log(computeSimpleMath(20, 5, "sum"));
 
 
 
-// ^^^^^^^^^^CHAPTER 3^^^^^^^^^^^^^
+// // ^^^^^^^^^^CHAPTER 3^^^^^^^^^^^^^
 
 
-//  CONDITIONAL STATEMENTS AND LOGIC
-// if else statements
-const age = 5; //data type number
-const myInput = "5"; // data type string
+// //  CONDITIONAL STATEMENTS AND LOGIC
+// // if else statements
+// const age = 5; //data type number
+// const myInput = "5"; // data type string
 
-if(age == myInput) {  //the condition in parenthesis with lineant equality sign
-  console.log("They are equal"); //if condition is true
-} else {
-  console.log("They are not equal"); // if condition is false
-}
+// if(age == myInput) {  //the condition in parenthesis with lineant equality sign
+//   console.log("They are equal"); //if condition is true
+// } else {
+//   console.log("They are not equal"); // if condition is false
+// }
 
-//To test strict equality
+// //To test strict equality
 
-if(age === myInput) { // strict equality sign
-  console.log("They are equal"); // if true
-} else {
-  console.log("They are not equal"); // if false
-}
+// if(age === myInput) { // strict equality sign
+//   console.log("They are equal"); // if true
+// } else {
+//   console.log("They are not equal"); // if false
+// }
 
-// if... else if statement(This allows us chain multiple conditions)
+// // if... else if statement(This allows us chain multiple conditions)
 
-if(certain condition) {
-  console.log("something") // if condition is true
-} else if(another condition) {
-  console.log("another thing")
-} else if(yet another condition) {
-  console.log("yet another thing")
-} else {
-  console.log("This thing rather") //if non of the above conditions evaluates to true
-}
-
-
-
-//ARRAYS AND ARRAY METHODS
-// Declare an array with 5 items
-var fruits = ["Pineapple", "Orange", "Mango", "Apple","Banana"];
-console.log(fruits);
-
-// Arrays are zero indexed, to access the 4th item in the fruits array, do This
-fruits[3];
-// Array can hold any data type
-
-// var mixedArray = [5,"How are you", true, "nature"];
-
-//ARRAY METHODS
-//Push to add to the end
-fruits.push("Pear");
-
-// Pop to remove an item from the beginning of fruits array
-fruits.pop();
-
-// Splice to remove any item from inside the fruit array
-
-fruits.splice(3, 1); //parameters are position and number of items
-
-// Slice to copy fruit array or extract some items from fruit to form another array
-
-fruits.slice();
-
-// Unshift to add an item to the beginning of fruits array
-fruits.unshift("Blackbery");
-
-// shift to remove an item from the beginning of fruits array
-fruits.shift();
+// if(certain condition) {
+//   console.log("something") // if condition is true
+// } else if(another condition) {
+//   console.log("another thing")
+// } else if(yet another condition) {
+//   console.log("yet another thing")
+// } else {
+//   console.log("This thing rather") //if non of the above conditions evaluates to true
+// }
 
 
-// Reverse to add an item to the beginning of fruits array
-fruits.reverse();
 
-// Join to convert an array into string
+// //ARRAYS AND ARRAY METHODS
+// // Declare an array with 5 items
+// var fruits = ["Pineapple", "Orange", "Mango", "Apple","Banana"];
+// console.log(fruits);
 
-var myNewString = fruits.join(", ");
-console.log(myNewString);
+// // Arrays are zero indexed, to access the 4th item in the fruits array, do This
+// fruits[3];
+// // Array can hold any data type
+
+// // var mixedArray = [5,"How are you", true, "nature"];
+
+// //ARRAY METHODS
+// //Push to add to the end
+// fruits.push("Pear");
+
+// // Pop to remove an item from the beginning of fruits array
+// fruits.pop();
+
+// // Splice to remove any item from inside the fruit array
+
+// fruits.splice(3, 1); //parameters are position and number of items
+
+// // Slice to copy fruit array or extract some items from fruit to form another array
+
+// fruits.slice();
+
+// // Unshift to add an item to the beginning of fruits array
+// fruits.unshift("Blackbery");
+
+// // shift to remove an item from the beginning of fruits array
+// fruits.shift();
+
+
+// // Reverse to add an item to the beginning of fruits array
+// fruits.reverse();
+
+// // Join to convert an array into string
+
+// var myNewString = fruits.join(", ");
+// console.log(myNewString);
 
 
