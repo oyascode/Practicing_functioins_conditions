5a2f11df2acc720ade0cb7c5334475df84111e26
oyascode <johnmikeloy@gmail.com>
2021-03-14 19:28:17 +0100

    Corrected function

--- script.js
+++ script.js
@@ -17,82 +17,25 @@ const computeSimpleMath = (firstNum, secondNum, myString) => {
 console.log(computeSimpleMath(20, 5, "sum"));
 
 
+// Corrected version of the function assignment...Always follow the instruction to the letter!!
 
-// // ^^^^^^^^^^CHAPTER 3^^^^^^^^^^^^^
-
-
-// //  CONDITIONAL STATEMENTS AND LOGIC
-// // if else statements
-// const age = 5; //data type number
-// const myInput = "5"; // data type string
-
-// if(age == myInput) {  //the condition in parenthesis with lineant equality sign
-//   console.log("They are equal"); //if condition is true
-// } else {
-//   console.log("They are not equal"); // if condition is false
-// }
-
-// //To test strict equality
-
-// if(age === myInput) { // strict equality sign
-//   console.log("They are equal"); // if true
-// } else {
-//   console.log("They are not equal"); // if false
-// }
-
-// // if... else if statement(This allows us chain multiple conditions)
-
-// if(certain condition) {
-//   console.log("something") // if condition is true
-// } else if(another condition) {
-//   console.log("another thing")
-// } else if(yet another condition) {
-//   console.log("yet another thing")
-// } else {
-//   console.log("This thing rather") //if non of the above conditions evaluates to true
-// }
-
-
-
-// //ARRAYS AND ARRAY METHODS
-// // Declare an array with 5 items
-// var fruits = ["Pineapple", "Orange", "Mango", "Apple","Banana"];
-// console.log(fruits);
-
-// // Arrays are zero indexed, to access the 4th item in the fruits array, do This
-// fruits[3];
-// // Array can hold any data type
-
-// // var mixedArray = [5,"How are you", true, "nature"];
-
-// //ARRAY METHODS
-// //Push to add to the end
-// fruits.push("Pear");
-
-// // Pop to remove an item from the beginning of fruits array
-// fruits.pop();
-
-// // Splice to remove any item from inside the fruit array
-
-// fruits.splice(3, 1); //parameters are position and number of items
-
-// // Slice to copy fruit array or extract some items from fruit to form another array
-
-// fruits.slice();
-
-// // Unshift to add an item to the beginning of fruits array
-// fruits.unshift("Blackbery");
-
-// // shift to remove an item from the beginning of fruits array
-// fruits.shift();
-
-
-// // Reverse to add an item to the beginning of fruits array
-// fruits.reverse();
-
-// // Join to convert an array into string
-
-// var myNewString = fruits.join(", ");
-// console.log(myNewString);
-
-
+//Declare a function with 3 parameters
+const computeSimpleMath = (firstNum, secondNum, myString) => { 
+  if(myString === "multiply" || myString === "Multiply") { //condition and case sensitivity check
+    const outcome = (firstNum * secondNum); //store the result in a outcome variable
+    return outcome;
+  } 
+  
+  if(myString === "divide" || myString === "Divide") {
+    const outcome = (firstNum / secondNum); //store the result in a outcome variable
+    return outcome;
+  }
+  
+  if(myString === "sum" || myString === "Sum") {
+    const outcome = (firstNum + secondNum); //store the result in a outcome variable
+    return outcome;
+  } 
+}
+//Invoke function with arguments and log to the console
+console.log(computeSimpleMath(20, 5, "sum"));
+console.log(computeSimpleMath(20, 5, "multiply"));
