30ed6f69e12223e120cac757ca3452e0d1ad68ef
oyascode <johnmikeloy@gmail.com>
2021-03-16 05:50:07 +0100

    object-array-assignment

---
+++ scripts/objects-arrays-assignments/objects-arrays-assignment.js
@@ -0,0 +1,112 @@
+// Create 3 realistic objects with at least 10 key-value pairs
+// One of the 3 objects should contain a key with another object value which in turn has 5 key-value pairs
+// Create 3 realistic arrays with at least 3 other realistic arrays as elements
+// Create an array with two realistic objects as its elements
+
+// OBJECT 1
+
+const musician = {
+  name: "Gril Surge",
+  age: 27,
+  Nationality: "Mexican",
+  musicGroup: "Trail Blazers",
+  numberOfAlbum: 60,
+  numberOfSongs: 1800,
+  musicStyle: "Blues",
+  instrumentsPlayed: 5;
+  voiceTone: "Baritone"
+  firstAlbumTitle: {
+    name: "Walking in the clouds",
+    releasedYear: 1989,
+    numberOfTracks: 14,
+    copiesSold: "50 Million",
+    preorderedCopies: "500 Thousand"
+  }
+}
+
+
+// OBJECT 2
+
+const lecturer = {
+  name: "John Doe",
+  field: "Science",
+  Department: "Physics",
+  subject: "Quantum Mechanics",
+  gender: "Male",
+  complexion: "Brown",
+  Age: 47,
+  email: "john_doe@yahoo.co.uk",
+  interest: ["Researching", "Reading", "sporting"],
+  lecturersCar: {
+    name: "Beatle",
+    model: 1987,
+    color: "Blue",
+    maximumSpeed: "120km/hr",
+    numberOfExhaust: 2
+  }
+}
+
+// OBJECT 3
+
+const vendor = {
+  companyName: "Crest Group",
+  nameOfFounder: {
+    firstName: "Carryington",
+    lastName: "Stuggard"
+  },
+  yearFounded: 1878,
+  numOfEmployees: 2000,
+  companySector: "FMCG",
+  companyProduct: "Drinks",
+  netWorth: "50 Million Dollars",
+  location: "Lagos",
+  annualProfit: "2 Million Dollars",
+  stillInOperation: true
+}
+
+
+// ARRAY 1...Plant array
+
+const plants = [
+  ['Hibiscus', 'Rose', 'Lily'], 
+  ['Cedar', 'Acacia', 'Cactus'], 
+  ['Grass', 'Herbs', 'Shrubs']
+];
+
+
+// ARRAY 2
+const animals = [
+  ['peacock', 'Ostrich', 'Swan'], 
+  ['Dog', 'Goat', 'Sheep'], 
+  ['Rat', 'Mouse', 'Squirrel']
+];
+
+
+// ARRAY 3
+const musicalInstrument = [
+  ['Clarinet', 'Oboe', 'Flute'], 
+  ['Microphone', 'Mixer', 'Amplifier'], 
+  ['Violin', 'Cello', 'Guitar']
+];
+
+
+// ARRAY OF TWO OBJECTS
+
+const superHeroes = [
+  {
+    name: "Spiderman",
+    modeOfFlight: "Spin webs",
+    active: true,
+    age: 23,
+    height: "5ft 6inch"
+  }, 
+  
+  {
+    name: "Superman",
+    modeOfFlight: "flay the cloak",
+    active: false,
+    age: 32,
+    height: "7ft"
+  }]
+
+
